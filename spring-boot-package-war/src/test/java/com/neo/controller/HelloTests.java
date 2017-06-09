8c7e2815f9048424d70c76f066c64df9ddba3e78
aslam <aslammaqbool@gmail.com>
2017-06-09 15:11:27 +0500

    updated to latest Spring Boot version

--- spring-boot-package-war/src/test/java/com/neo/controller/HelloTests.java
+++ spring-boot-package-war/src/test/java/com/neo/controller/HelloTests.java
@@ -3,9 +3,7 @@ package com.neo.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
 public class HelloTests {
 
@@ -33,7 +30,7 @@ public class HelloTests {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World")));
+                .andExpect(content().string(equalTo("Hello World - war packaging")));
     }
 
 }
--- spring-boot-package-war/src/test/java/com/neo/controller/HelloWorldControlerTests.java
+++ spring-boot-package-war/src/test/java/com/neo/controller/HelloWorldControlerTests.java
@@ -3,9 +3,7 @@ package com.neo.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,7 +13,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
 @WebAppConfiguration
 public class HelloWorldControlerTests {
 
